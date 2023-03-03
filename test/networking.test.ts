67309ce0e2d97818a4591281aed396c277735a94
Julio Castillo <121035941+owitech@users.noreply.github.com>
2023-03-02 23:01:57 -0500

    Simple config vpc

---
+++ test/networking.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Networking from '../lib/networking-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/networking-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Networking.NetworkingStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
